493881d5321c826cbf990ec89b8e44b7bfa162c4
Sanjay <hrsanjay@gmail.com>
2026-03-10 21:41:33 +0530

    first commit

---
+++ syntax.py
@@ -0,0 +1,12 @@
+#total number of keywords in python
+import keyword
+print(keyword.kwlist)
+print("Total number of keywords:", len(keyword.kwlist))
+
+#syntax of python
+print("Hello, World! ")
+
+print("namaste")
+print("ನಮಸ್ತೆ")
+a="sanju"
+print(a)
---
+++ variable.py
@@ -0,0 +1,41 @@
+a=12    #int
+b=12.4    #float(decimal number)
+c="sanju"    #string
+d=True    #boolean
+e=None    #None
+f=1+2j    #complex
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+print(e)
+print(type(e))
+print(f)
+print(type(f))
+
+#type casting
+#String to Integer
+a = "10"
+b = int(a)
+
+print(b)
+print(type(b))
+
+#Integer to Float
+a = 5
+b = float(a)
+
+print(b)
+print(type(b))
+
+#Integer to String
+a = 100
+b = str(a)
+
+print(b)
+print(type(b))
+#
