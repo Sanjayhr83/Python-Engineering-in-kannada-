e0536a0d5c9340b5cb116a3c75bc74f311aa387f
Sanjay <hrsanjay@gmail.com>
2026-03-17 22:49:54 +0530

    Rock,Paper,Scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,27 +1,28 @@
-import random
-choices= ["rock", "paper", "scissors"]
+while True:
+    import random
+    choices= ["rock", "paper", "scissors"]
 
-computer = random.choice(choices)
+    computer = random.choice(choices)
 
-user = input("Enter your choice: ").lower()
+    user = input("Enter your choice: ").lower()
 
-print("Computer choice: ", computer)
+    print("Computer choice: ", computer)
 
-if user == computer:
-    print("It's a tie!")
-elif user == "rock":
-    if computer == "scissors":
-        print("You win!")
+    if user == computer:
+        print("It's a tie!")
+    elif user == "rock":
+        if computer == "scissors":
+            print("You win!")
+        else:
+            print("You lose!")
+    elif user == "paper":
+        if computer == "rock":
+            print("You win!")
+        else:
+            print("You lose!")
+    elif user == "scissors":
+        if computer == "paper":
+            print("You win!")
     else:
-        print("You lose!")
-elif user == "paper":
-    if computer == "rock":
-        print("You win!")
-    else:
-        print("You lose!")
-elif user == "scissors":
-    if computer == "paper":
-        print("You win!")
-else:
-    print("Invalid input! Please choose rock, paper, or scissors.")
+        print("Invalid input! Please choose rock, paper, or scissors.")
 
