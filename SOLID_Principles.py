5a30ab62659b249a82c7f9217a4149cc544583aa
Sanjay <hrsanjay@gmail.com>
2026-03-20 12:19:59 +0530

    SOLID principles

--- SOLID_Principles.py
+++ SOLID_Principles.py
@@ -57,3 +57,101 @@ class RegularCustomer(Discount):
 class PremiumCustomer(Discount):
     def get_discount(self):
         return 20
+
+# ✅ 3. Liskov Substitution Principle (LSP)
+# Definition: Subclasses should be able to replace their parent class without breaking the program.
+
+# ❌ Bad Example:
+class Bird:
+    def fly(self):
+        print("Flying...")
+
+class Penguin(Bird):
+    def fly(self):
+        raise NotImplementedError("Penguins can't fly")
+
+# ✅ Good Example:
+class Bird:
+    def move(self):
+        pass
+
+class Sparrow(Bird):
+    def move(self):
+        print("Flying...")
+
+class Penguin(Bird):
+    def move(self):
+        print("Swimming...")
+
+
+# ✅ 4. Interface Segregation Principle (ISP)
+# Definition: Don’t force a class to implement methods it does not use.
+# Python doesn’t have interfaces like Java/C#, but we can still follow this idea using base classes.
+
+# ❌ Bad Example:
+class Worker:
+    def work(self):
+        pass
+
+    def eat(self):
+        pass
+
+class Robot(Worker):
+    def eat(self):
+        raise NotImplementedError("Robots don't eat")
+
+# ✅ Good Example:
+class Workable:
+    def work(self):
+        pass
+
+class Eatable:
+    def eat(self):
+        pass
+
+class Human(Workable, Eatable):
+    def work(self):
+        print("Human working")
+
+    def eat(self):
+        print("Human eating")
+
+class Robot(Workable):
+    def work(self):
+        print("Robot working")
+
+
+# ✅ 5. Dependency Inversion Principle (DIP)
+# Definition: High-level modules should not depend on low-level modules. Both should depend on abstractions.
+
+# ❌ Bad Example:
+class Keyboard:
+    def input(self):
+        return "User typing..."
+
+class Computer:
+    def __init__(self):
+        self.keyboard = Keyboard()
+
+    def get_input(self):
+        return self.keyboard.input()
+
+#✅ Good Example:
+class InputDevice:
+    def input(self):
+        pass
+
+class Keyboard(InputDevice):
+    def input(self):
+        return "User typing..."
+
+class Mouse(InputDevice):
+    def input(self):
+        return "Mouse clicked"
+
+class Computer:
+    def __init__(self, device: InputDevice):
+        self.device = device
+
+    def get_input(self):
+        return self.device.input()
