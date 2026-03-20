1cc87f4590e503ac4323fa9ce12a883bd9209474
Sanjay <hrsanjay@gmail.com>
2026-03-20 13:05:44 +0530

    Error Handling

---
+++ error_handling.py
@@ -0,0 +1,21 @@
+#what is error:An error is a program that prevents it from running correctly
+
+"""
+Types of Error:
+1. Syntax Error:Mistakes in the code that violate the rules of the programming language.
+ex:
+print("Hello"
+o/p:❌ Missing ) → SyntaxError
+
+2. Runtime Error:Errors that occur during the execution of a program.
+ex:
+print(10 / 0)
+o/p:❌ Division by zero → ZeroDivisionError
+
+3. Logical Error:Errors that occur when the program does not produce the expected output.
+ex:
+print(2+2)
+o/p:❌ expecting 5 (wrong logic)
+"""
+
+
